744f878b5b579e47d868ddd1c6d59cb0df8f6490
Damilare Jolayemi <djolayemi@gmail.com>
2021-10-22 14:29:20 +0100

    auth request model

---
+++ src/main/java/com/dami/springsecurityjwt/model/AuthenticationRequest.java
@@ -0,0 +1,14 @@
+package com.dami.springsecurityjwt.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+
+    public String username;
+    public String password;
+}
